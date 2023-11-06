a5e87cae2c12de5f412da37a582901344c58b498
JasperdeBeijer <1197893@student.roc-nijmegen.nl>
2023-11-06 14:03:10 +0100

    bezig met frontend homepage, header gemaakt en in includes gezet

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+
+<div class="navbar">
+    <div class="navlinks">
+        <a href="./"><p>Logo</p></a>
+    </div>
+    <div class="navrechts">
+        <a href="./"><p>Home</p></a>
+        <a href="./auth/register.php"><p>Register</p></a>
+        <a href="./auth/login.php"><p>Login</p></a>
+        <a href="./klant/index.php"><p>Account</p></a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,6 +7,26 @@
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    
+    <div class="pagina">
+        <?php include './includes/header.php';?>
+        <div class="content">
+            <div class="boven">
+                <div class="bovenlinks">
+                    <p>Image</p>
+                </div>
+                <div class="bovenrechts">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In laboriosam accusantium tenetur libero omnis obcaecati nemo a! Officiis ullam laudantium voluptatem debitis. Ullam itaque sequi nihil tenetur. Quibusdam, quis accusamus.</p>
+                </div>
+            </div>
+            <div class="onder">
+                <div class="onderlinks">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In laboriosam accusantium tenetur libero omnis obcaecati nemo a! Officiis ullam laudantium voluptatem debitis. Ullam itaque sequi nihil tenetur. Quibusdam, quis accusamus.</p>
+                </div>
+                <div class="onderrechts">
+                    <p>Image</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
